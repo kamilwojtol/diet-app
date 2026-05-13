d9e3a44fb1370c37e19169002ebcfa0bef1cb636
Kamil Wojtol <kamil.wojtol97@gmail.com>
2026-05-13 20:39:35 +0200

    initial commit

---
+++ apps/backend/src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API works!" });
+});
+
+app.listen(5000, () => {
+  console.log("Server is running");
+});
